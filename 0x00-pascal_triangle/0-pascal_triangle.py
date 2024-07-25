454a56251a96564f885b1470d4913c2c4b2ac914
kofiad <kofiadaba@gmail.com>
2024-07-25 18:46:15 +0000

    Refactor pascal_triangle function for improved efficiency and readability

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,7 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal's triangle"""
+    """Returns the Pascal's triangle"""
     if n <= 0:
         return []
     triangle = [[1]]
